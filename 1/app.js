23eaaee877cd6f105a0969762d7412de1579e48d
lkh <sdlrudgh@gmail.com>
2022-02-11 20:44:43 +0900

    파일정리

--- 1/app.js
+++ 1/app.js
@@ -1,38 +1,70 @@
-var fs = require('fs');
-const path=require('path');
-const { setFlagsFromString } = require('v8');
-const folder=process.argv[2];
-if(!folder||fs.existsSync(workingDir)){
-    console.error('plz enter foler name in picture');
-    return;
-}
-const workingDir=path.join(os.homedir(),'Pictures',folder)
-console.log(workingDir);
-var filePath="C:/test";
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-const viedoDir=path.join(workingDir,'viedo');
-const capturedDir=path.join(workingDir,'captured');
-const duplicatedDir=path.join(workingDir,'duplicated');
-
-fs.mkdirSync(videoDir);
-fs.mkdirSync(capturedDir);
-fs.mkdirSync(duplicatedDir);
-
-fs.promises.readdir(workingDir).then(processFoles)
-
-})}
-
-function processFoles(files){
-    files.forEach((file)=>{
-        if(isVideoFile(file)){
-            console.log('video',file)
-        }else if(isCapturedFile(file)){
-
-        }else if(isDuplicatedFile(file)){
-
-        }
-    });
+// 
+// 1
+const folder = process.argv[2];
+const workingDir = path.join(os.homedir(), 'Pictures', folder);
+if (!folder || !fs.existsSync(workingDir)) {
+  console.error('Please enter folder name in Pictures');
+  return;
 }
 
+// 2
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
 
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
 
+// 3
+fs.promises
+  .readdir(workingDir) 
+  .then(processFiles)
+  .catch(console.log);
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      move(file, videoDir);
+    } else if (isCapturedFile(file)) {
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+
+function isDuplicatedFile(files, file) {
+  if (!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+    return false;
+  }
+
+  const edited = `IMG_E${file.split('_')[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  console.info(`move ${file} to ${path.basename(targetDir)}`);
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises
+    .rename(oldPath, newPath) //
+    .catch(console.error);
+}
